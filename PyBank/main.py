46d7c911d433ffd6468dc6747ee28dbe138571da
denk-ie <d.okur@hotmail.com>
2022-05-20 20:06:48 +1000

    added main.py in PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+output_path = os.path.join('analysis', 'analysis.txt')
+
+print("Financial Analysis")
+print("----------------------------")
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    print(csvreader)
+    
+    csv_header = next(csvreader)
+    
+    #for row in csvreader:
